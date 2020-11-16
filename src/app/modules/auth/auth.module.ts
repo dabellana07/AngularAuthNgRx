e049325a3f6eb71cd2d21d24cac657144ffaac6d
Destiny Abellana <dabellana@arcanys.com>
2020-11-16 14:33:40 +0800

    Import routing module

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { AuthRoutingModule } from './auth-routing.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    AuthRoutingModule,
+    SharedModule
   ]
 })
 export class AuthModule { }
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { HomeRoutingModule } from './home-routing.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    HomeRoutingModule,
+    SharedModule
   ]
 })
 export class HomeModule { }
