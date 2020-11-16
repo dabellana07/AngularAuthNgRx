69503499672e5f3c0a6c2ed9148c7cf9dfbcd557
Destiny Abellana <dabellana@arcanys.com>
2020-11-16 16:27:40 +0800

    Added AuthLayout and ContentLayout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,21 @@ import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { EffectsModule } from '@ngrx/effects';
+import { CoreModule } from './core/core.module';
+import { HomeModule } from './modules/home/home.module';
+import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
+import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthLayoutComponent,
+    ContentLayoutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    CoreModule,
     StoreModule.forRoot({}, {}),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
     EffectsModule.forRoot([])
