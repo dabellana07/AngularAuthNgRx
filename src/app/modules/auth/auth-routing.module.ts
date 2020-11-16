c550d88e144f24aee104c5c9a0180ce3a82c008c
Destiny Abellana <dabellana@arcanys.com>
2020-11-16 18:33:44 +0800

    Removed unused import

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
-import { AuthGuard } from 'src/app/core/guard/auth.guard';
 import { SignedInGuard } from 'src/app/core/guard/signed-in.guard';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
