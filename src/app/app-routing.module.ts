12489b6cd58e32065f8abdc201a62c7b83baf4bd
Destiny Abellana <dabellana@arcanys.com>
2020-11-16 16:41:46 +0800

    Add login and register component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
+import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthLayoutComponent,
+    loadChildren: () =>
+        import('./modules/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: '',
+    component: ContentLayoutComponent,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () =>
+            import('./modules/home/home.module').then(m => m.HomeModule)
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent, RegisterComponent],
   imports: [
     AuthRoutingModule,
     SharedModule
